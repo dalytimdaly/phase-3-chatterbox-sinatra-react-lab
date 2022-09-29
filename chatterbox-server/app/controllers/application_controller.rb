18086421457a36ea9995304a943cef637829e153
Tim <dalytimdaly@gmail.com>
2022-09-29 12:08:36 -0600

    finished

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -2,5 +2,31 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add routes
+  get '/messages' do
+      messages = Message.all
+      messages.to_json
+  end
+
+  post '/messages' do
+    newmessage = Message.create(
+    body: params[:body],
+    username: params[:username]
+  )
+    newmessage.to_json
+  end
+
+  patch '/messages/:id' do
+    patchmessage = Message.find(params[:id])
+    patchmessage.update(
+      body: params[:body]
+    )
+    patchmessage.to_json
+  end
+
+  delete '/messages/:id' do
+    deletemessage = Message.find(params[:id])
+    deletemessage.destroy
+    deletemessage.to_json
+  end
   
 end
